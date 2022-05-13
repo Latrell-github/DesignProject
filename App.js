14a5dbad06b23ae4a6b2e8b5e504c310926a611c
Latrell Costales <latrellcostales59@gmail.com>
2022-05-13 08:38:39 +0800

    try

--- App.js
+++ App.js
@@ -4,7 +4,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open </Text>
+      <Text>Open kisame </Text>
       <StatusBar style="auto" />
     </View>
   );
